1efd7cde5b5088ee2c522a03001e4ee4141e3d3e
salvadordsf <salvadordisabatto@gmail.com>
2026-04-16 12:16:25 -0300

    feat(main-stats): create the status card comp for main vars

---
+++ src/features/main-stats/components/main-var-state-card.tsx
@@ -0,0 +1,36 @@
+import { clsx } from "clsx";
+import { ReactElement } from "react";
+
+export const MainVarStateCard = ({
+  stateColor,
+  title,
+  children,
+}: {
+  stateColor: "red" | "yellow" | "green";
+  title: string;
+  children: ReactElement;
+}) => {
+  const cardClass =
+  stateColor === "red"
+    ? "border-red-600 bg-red-600/20"
+    : stateColor === "yellow"
+    ? "border-yellow-600 bg-yellow-600/20"
+    : "border-green-800 bg-green-800/20";
+
+    const textClass =
+  stateColor === "red"
+    ? "text-red-300"
+    : stateColor === "yellow"
+    ? "text-yellow-300"
+    : "text-green-300";
+
+    console.log(cardClass)
+  return (
+    <div className={clsx("flex flex-col items-center gap-1 w-90 p-4 border rounded-xl", cardClass)}>
+      <h3 className="text-white font-bold">{title}</h3>
+      <div className={textClass}>
+        {children}
+      </div>
+    </div>
+  );
+};
