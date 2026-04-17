b8fdf841583a2da5813b06438f604a337984db25
salvadordsf <salvadordisabatto@gmail.com>
2026-04-17 18:30:53 -0300

    feat(main-status): create the deposit-dolarization card, gir card and real rate in ARS card

---
+++ src/features/main-stats/components/cards/extern-sector-finance/deposit-dolarization-card.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import { useLastGIR } from "@/src/services/GIR/services/gir-querys";
+import { useEffect, useState } from "react";
+import { MainVarCardSkeleton } from "../../main-var-card-skeleton";
+import { MainVarStateCard } from "../main-var-state-card";
+import {
+  useARSDeposits,
+  useMEDeposits,
+} from "@/src/services/DEPOSITS/services/deposits-querys";
+
+export const DepositDolarizationCard = () => {
+  // Fetch ARS deposits record
+  const {
+    data: ARSDeposits,
+    isLoading: isLoadingARSDeposits,
+    isError: isErrorARSDeposits,
+  } = useARSDeposits();
+  const {
+    data: MEDeposits,
+    isLoading: isLoadingMEDeposits,
+    isError: isErrorMEDeposits,
+  } = useMEDeposits();
+
+  const [depositsStatus, setDepositsStatus] = useState<
+    "red" | "yellow" | "green" | null
+  >(null);
+  const [deposits, setDeposits] = useState<number | null>(null);
+
+  // Set the status (red | yellow | green)
+  useEffect(() => {
+    const depositsDolarization =
+      ARSDeposits && MEDeposits
+        ? (MEDeposits.value / (ARSDeposits.value + MEDeposits.value)) * 100
+        : null;
+    if (depositsDolarization && depositsDolarization !== null) {
+      setDeposits(depositsDolarization);
+      setDepositsStatus(
+        depositsDolarization > 45
+          ? "red"
+          : depositsDolarization > 30
+            ? "yellow"
+            : "red",
+      );
+    }
+  }, [MEDeposits, ARSDeposits, setDepositsStatus, setDeposits]);
+
+  if (isLoadingARSDeposits || isLoadingMEDeposits)
+    return <MainVarCardSkeleton />;
+  if (isErrorARSDeposits || isErrorMEDeposits || !depositsStatus || !deposits)
+    return <p>Error al cargar depositos</p>;
+  return (
+    <MainVarStateCard
+      stateColor={depositsStatus}
+      title="Dolarización de depósitos del sector privado"
+    >
+      <div className="flex flex-col items-center">
+        <span className="text-xl font-bold">{deposits.toFixed()}%</span>
+        <span className="text-xs text-neutral-400  font-extralight">
+          Fecha: {ARSDeposits?.labels.lastDate}
+        </span>
+      </div>
+    </MainVarStateCard>
+  );
+};
---
+++ src/features/main-stats/components/cards/extern-sector-finance/real-rate-ARS-card.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import { MainVarStateCard } from "../main-var-state-card";
+import { useEffect, useState } from "react";
+import { useLastInflation } from "@/src/services/INFLATION/services/inflation-query";
+import { useLastBADLAR } from "@/src/services/BADLAR/services/badlar-querys";
+import { getTRM } from "../../../../economic-calcs/badlar-calcs";
+import { MainVarCardSkeleton } from "../../main-var-card-skeleton";
+
+export const RealRateARSCard = () => {
+  // Fetch last Inflation record
+  const {
+    data: lastInflation,
+    isLoading: isLoadingInflation,
+    isError: isErrorInflation,
+  } = useLastInflation();
+  // Fetch last BADLAR record
+  const {
+    data: lastBADLAR,
+    isLoading: isLoadingBADLAR,
+    isError: isErrorBADLAR,
+  } = useLastBADLAR();
+
+  const [realRate, setRealRate] = useState<null | number>(null);
+  const [realRateStatus, setRealRateStatus] = useState<
+    "red" | "yellow" | "green" | null
+  >(null);
+
+  // Set the status (red | yellow | green)
+  useEffect(() => {
+    const realRate =
+      lastInflation && lastBADLAR
+        ? getTRM(lastBADLAR.value, lastInflation.value)
+        : null;
+    if (realRate) setRealRate(realRate);
+    setRealRateStatus(
+      realRate! > 0 ? "green" : realRate! > -2 ? "yellow" : "red",
+    );
+  }, [lastInflation, lastBADLAR, setRealRateStatus]);
+
+  if (isLoadingInflation || isLoadingBADLAR)
+    return <MainVarCardSkeleton />;
+  if (
+    isErrorInflation ||
+    isErrorBADLAR ||
+    !lastInflation ||
+    !lastBADLAR ||
+    !realRateStatus
+  )
+    return <p>Error al cargar inflacion</p>;
+  return (
+    <MainVarStateCard stateColor={realRateStatus} title="Tasa real mensual en pesos">
+      <div className="flex flex-col items-center gap-2">
+        <span className="text-xl font-bold">{realRate?.toFixed(3)}%</span>
+        <div className="flex flex-col items-center">
+          <span className="text-xs text-neutral-400  font-extralight">
+            BADLAR mensualizada − inflación mensual
+          </span>
+          <span className="text-xs text-neutral-400  font-extralight">
+            Fecha: {lastBADLAR.labels.lastDate}
+          </span>
+        </div>
+      </div>
+    </MainVarStateCard>
+  );
+};
