383bf4a3aad82902c865de583199a27d131c93d3
salvadordsf <salvadordisabatto@gmail.com>
2026-04-17 18:29:43 -0300

    feat(main-status): create the services and types for USDs

--- src/services/USD/services/usd-query.ts
+++ src/services/USD/services/usd-query.ts
@@ -1,5 +1,17 @@
 import { useQuery } from "@tanstack/react-query";
-import { getUSDVariation } from "./usd-axios";
+import { getLastUSDExchange, getUSDVariation } from "./usd-axios";
+
+export const useUSDSExchange = () => {
+  return useQuery({
+    queryKey: ["usdsExchange"],
+    queryFn: getLastUSDExchange,
+
+    staleTime: 1000 * 60 * 60 * 24,
+    gcTime: 1000 * 60 * 60 * 24,
+    retry: 2,
+    refetchOnWindowFocus: false,
+  });
+};
 
 export const useUSDVariation = () => {
   return useQuery({
