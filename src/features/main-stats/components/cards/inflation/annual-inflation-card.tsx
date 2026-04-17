e84b4a1a3f415c6221c62b207c9bdecf8dd5575d
salvadordsf <salvadordisabatto@gmail.com>
2026-04-17 18:19:36 -0300

    feat(main-status): create the annual inflatio card

---
+++ src/features/main-stats/components/cards/inflation/annual-inflation-card.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { MainVarStateCard } from "../main-var-state-card";
+import { useEffect, useState } from "react";
+import { MainVarCardSkeleton } from "../../main-var-card-skeleton";
+import { useLastAnnualInflation } from "@/src/services/INFLATION/services/inflation-query";
+
+export const AnnualInflationCard = () => {
+  // Fetch last Annual Inflation record
+  const {
+    data: lastAnualInflation,
+    isLoading,
+    isError,
+  } = useLastAnnualInflation();
+  const [inflationStatus, setInflationStatus] = useState<
+    "red" | "yellow" | "green" | null
+  >(null);
+
+  // Set the status (red | yellow | green)
+  useEffect(() => {
+    const inflationValue = lastAnualInflation ? lastAnualInflation.value : null;
+    if (inflationValue)
+      setInflationStatus(
+        inflationValue > 80 ? "red" : inflationValue > 30 ? "yellow" : "green",
+      );
+  }, [lastAnualInflation, setInflationStatus]);
+
+  if (isLoading) return <MainVarCardSkeleton />;
+  if (isError || !lastAnualInflation || !inflationStatus)
+    return <p>Error al cargar inflacion interanual</p>;
+  return (
+    <MainVarStateCard
+      stateColor={inflationStatus}
+      title={lastAnualInflation.title}
+    >
+      <div className="flex flex-col items-center">
+        <span className="text-xl">
+          <span className="font-bold">{lastAnualInflation.value} </span>
+          {lastAnualInflation.labels.unit}
+        </span>
+        <span className="text-xs text-neutral-400  font-extralight">
+          Fecha: {lastAnualInflation.labels.lastDate}
+        </span>
+      </div>
+    </MainVarStateCard>
+  );
+};
