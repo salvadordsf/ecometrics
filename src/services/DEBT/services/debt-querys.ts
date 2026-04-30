215ea1311007548a2c3396275ac5be49f6d728f3
salvadordsf <salvadordisabatto@gmail.com>
2026-04-30 10:23:19 -0300

    feat(debt): usePrivateDebt hook was created
    - The usePrivateDebt hook was created to fetch the history of all types of debt in local, foreign and total currency with a limit prop.

--- src/services/DEBT/services/debt-querys.ts
+++ src/services/DEBT/services/debt-querys.ts
@@ -1,10 +1,22 @@
 import { useQuery } from "@tanstack/react-query";
-import { getLastPrivateDebts } from "./debt-axios";
+import { getLastPrivateDebts, getPrivateDebts } from "./debt-axios";
 
 export const useLastPrivateDebt = () => {
   return useQuery({
     queryKey: ["lastPrivateDebt"],
     queryFn: getLastPrivateDebts,
+
+    staleTime: 1000 * 60 * 60 * 24,
+    gcTime: 1000 * 60 * 60 * 24,
+    retry: 2,
+    refetchOnWindowFocus: false,
+  });
+};
+
+export const usePrivateDebt = (limit = 3000) => {
+  return useQuery({
+    queryKey: ["privateDebt", limit],
+    queryFn: () => getPrivateDebts(limit),
 
     staleTime: 1000 * 60 * 60 * 24,
     gcTime: 1000 * 60 * 60 * 24,
